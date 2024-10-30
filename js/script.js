14e95b802fdfcd092a19b65c0388e40c0c41f203
Code-Verse <141191909+Maiku265@users.noreply.github.com>
2024-10-30 16:54:02 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -227,3 +227,18 @@ document.addEventListener("DOMContentLoaded", () => {
     updateBatteryStatus();
     fetchWeather();
 });
+if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+          navigator.serviceWorker
+            .register("sw.js")
+            .then((registration) => {
+              console.log(
+                "ServiceWorker registration successful:",
+                registration
+              );
+            })
+            .catch((error) => {
+              console.log("ServiceWorker registration failed:", error);
+            });
+        });
+      }
